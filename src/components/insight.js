384956489c3b33fc759fe51d1365040d33d6c1f9
aidan-parkinson <aidan.parkinson@gmail.com>
2021-06-08 19:59:12 +0100

    ux polish

--- src/components/insight.js
+++ src/components/insight.js
@@ -13,25 +13,25 @@ class Insight extends Component {
           </p>
           <ul>
             <li class="nav-item">
-              <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2017.1314148?journalCode=rbri20" class="nav-link">Evaluating positivist theories of occupant satisfaction: a statistical analysis, 2017</a>
+              <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2017.1314148?journalCode=rbri20"><font color="MediumSeaGreen" size="1.2rem">Evaluating positivist theories of occupant satisfaction: a statistical analysis, 2017</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.sciencedirect.com/science/article/abs/pii/S0360544216302638" class="nav-link">An income-based analysis of the value premise for property energy performance, 2016</a>
+              <a href="https://www.sciencedirect.com/science/article/abs/pii/S0360544216302638"><font color="MediumSeaGreen" size="1.2rem">An income-based analysis of the value premise for property energy performance, 2016</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.repository.cam.ac.uk/handle/1810/261155" class="nav-link">An exploration of building energy performance and financial value with demonstration on UK offices, 2016</a>
+              <a href="https://www.repository.cam.ac.uk/handle/1810/261155"><font color="MediumSeaGreen" size="1.2rem">An exploration of building energy performance and financial value with demonstration on UK offices, 2016</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.sciencedirect.com/science/article/pii/S0306261914007570?via%3Dihub" class="nav-link">Evaluating the energy performance of buildings within a value at risk framework with demonstration on UK offices, 2014</a>
+              <a href="https://www.sciencedirect.com/science/article/pii/S0306261914007570?via%3Dihub"><font color="MediumSeaGreen" size="1.2rem">Evaluating the energy performance of buildings within a value at risk framework with demonstration on UK offices, 2014</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.sciencedirect.com/science/article/pii/S0301421513006861" class="nav-link">Energy performance certification as a signal of workplace quality, 2013</a>
+              <a href="https://www.sciencedirect.com/science/article/pii/S0301421513006861"><font color="MediumSeaGreen" size="1.2rem">Energy performance certification as a signal of workplace quality, 2013</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2012.690956" class="nav-link">Exploring scenarios for the future of energy management in UK property, 2012</a>
+              <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2012.690956"><font color="MediumSeaGreen" size="1.2rem">Exploring scenarios for the future of energy management in UK property, 2012</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://link.springer.com/chapter/10.1007/978-3-642-27509-8_7" class="nav-link">Market Responses to the Sustainability and Energy Performance of Commercial Property, 2012</a>
+              <a href="https://link.springer.com/chapter/10.1007/978-3-642-27509-8_7"><font color="MediumSeaGreen" size="1.2rem">Market Responses to the Sustainability and Energy Performance of Commercial Property, 2012</font></a>
             </li>
           </ul>
         </div>
@@ -39,22 +39,22 @@ class Insight extends Component {
           <p><font color="Tomato" size="1.2rem">SCIENTIFIC REFEREEING</font></p>
           <ul>
             <li class="nav-item">
-              <a href="https://www.journals.elsevier.com/energy" class="nav-link">Energy</a>
+              <a href="https://www.journals.elsevier.com/energy"><font color="Tomato" size="1.2rem">Energy</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.journals.elsevier.com/energy-policy" class="nav-link">Energy Policy</a>
+              <a href="https://www.journals.elsevier.com/energy-policy"><font color="Tomato" size="1.2rem">Energy Policy</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.journals.elsevier.com/applied-energy" class="nav-link">Applied Energy</a>
+              <a href="https://www.journals.elsevier.com/applied-energy"><font color="Tomato" size="1.2rem">Applied Energy</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.journals.elsevier.com/sustainable-cities-and-society" class="nav-link">Sustainable Cities and Society</a>
+              <a href="https://www.journals.elsevier.com/sustainable-cities-and-society"><font color="Tomato" size="1.2rem">Sustainable Cities and Society</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.journals.elsevier.com/energy-economics" class="nav-link">Energy Economics</a>
+              <a href="https://www.journals.elsevier.com/energy-economics"><font color="Tomato" size="1.2rem">Energy Economics</font></a>
             </li>
             <li class="nav-item">
-              <a href="https://www.journals.elsevier.com/journal-of-environmental-management" class="nav-link">Journal of Environmental Management</a>
+              <a href="https://www.journals.elsevier.com/journal-of-environmental-management"><font color="Tomato" size="1.2rem">Journal of Environmental Management</font></a>
             </li>
           </ul>
         </div>
