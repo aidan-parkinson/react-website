85ebbbad5cd40423e882a010a3299be5e1e1534e
aidanparkinson <aidan.parkinson@arup.com>
2021-06-08 19:26:00 +0100

    css

--- src/components/insight.js
+++ src/components/insight.js
@@ -2,75 +2,62 @@
 
 import React, { Component } from 'react'
 
-const divStyle = {
-  margin : "20px",
-  fontSize: "0.8rem"
-};
-
-const linkStyle = {
-  color: "#2d2d2d",
-  fontSize: "0.8rem"
-};
-
-const parStyle = {
-  color: "#2d2d2d",
-  textAlign: "justify",
-  margin: "20px",
-  fontSize: "0.9rem",
-  maxWidth: "930px"
-}
-
 class Insight extends Component {
   render() {
     return (
       <div>
-        <p style={parStyle}>
-          Scientific publications
-        </p>
-        <ul>
-          <li class="nav-item">
-            <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2017.1314148?journalCode=rbri20" class="nav-link">Evaluating positivist theories of occupant satisfaction: a statistical analysis, 2017</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.sciencedirect.com/science/article/abs/pii/S0360544216302638" class="nav-link">An income-based analysis of the value premise for property energy performance, 2016</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.repository.cam.ac.uk/handle/1810/261155" class="nav-link">An exploration of building energy performance and financial value with demonstration on UK offices, 2016</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.sciencedirect.com/science/article/pii/S0306261914007570?via%3Dihub" class="nav-link">Evaluating the energy performance of buildings within a value at risk framework with demonstration on UK offices, 2014</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.sciencedirect.com/science/article/pii/S0301421513006861" class="nav-link">Energy performance certification as a signal of workplace quality, 2013</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2012.690956" class="nav-link">Exploring scenarios for the future of energy management in UK property, 2012</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://link.springer.com/chapter/10.1007/978-3-642-27509-8_7" class="nav-link">Market Responses to the Sustainability and Energy Performance of Commercial Property, 2012</a>
-          </li>
-        </ul>
-        <p style={parStyle}>Scientific refereeing</p>
-        <ul>
-          <li class="nav-item">
-            <a href="https://www.journals.elsevier.com/energy" class="nav-link">Energy</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.journals.elsevier.com/energy-policy" class="nav-link">Energy Policy</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.journals.elsevier.com/applied-energy" class="nav-link">Applied Energy</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.journals.elsevier.com/sustainable-cities-and-society" class="nav-link">Sustainable Cities and Society</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.journals.elsevier.com/energy-economics" class="nav-link">Energy Economics</a>
-          </li>
-          <li class="nav-item">
-            <a href="https://www.journals.elsevier.com/journal-of-environmental-management" class="nav-link">Journal of Environmental Management</a>
-          </li>
-        </ul>
+        <div class="green-insight">
+          <p><font color="MediumSeaGreen" size="1.2rem">
+            SCIENTIFIC PUBLICATIONS
+            </font>
+          </p>
+          <ul>
+            <li class="nav-item">
+              <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2017.1314148?journalCode=rbri20" class="nav-link">Evaluating positivist theories of occupant satisfaction: a statistical analysis, 2017</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.sciencedirect.com/science/article/abs/pii/S0360544216302638" class="nav-link">An income-based analysis of the value premise for property energy performance, 2016</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.repository.cam.ac.uk/handle/1810/261155" class="nav-link">An exploration of building energy performance and financial value with demonstration on UK offices, 2016</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.sciencedirect.com/science/article/pii/S0306261914007570?via%3Dihub" class="nav-link">Evaluating the energy performance of buildings within a value at risk framework with demonstration on UK offices, 2014</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.sciencedirect.com/science/article/pii/S0301421513006861" class="nav-link">Energy performance certification as a signal of workplace quality, 2013</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.tandfonline.com/doi/abs/10.1080/09613218.2012.690956" class="nav-link">Exploring scenarios for the future of energy management in UK property, 2012</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://link.springer.com/chapter/10.1007/978-3-642-27509-8_7" class="nav-link">Market Responses to the Sustainability and Energy Performance of Commercial Property, 2012</a>
+            </li>
+          </ul>
+        </div>
+        <div class="red-insight">
+          <p><font color="Tomato" size="1.2rem">SCIENTIFIC REFEREEING</font></p>
+          <ul>
+            <li class="nav-item">
+              <a href="https://www.journals.elsevier.com/energy" class="nav-link">Energy</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.journals.elsevier.com/energy-policy" class="nav-link">Energy Policy</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.journals.elsevier.com/applied-energy" class="nav-link">Applied Energy</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.journals.elsevier.com/sustainable-cities-and-society" class="nav-link">Sustainable Cities and Society</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.journals.elsevier.com/energy-economics" class="nav-link">Energy Economics</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.journals.elsevier.com/journal-of-environmental-management" class="nav-link">Journal of Environmental Management</a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/components/introduction.js
+++ src/components/introduction.js
@@ -5,72 +5,73 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Timeline } from 'react-twitter-widgets'
 
-const divStyle = {
-  margin : "20px",
-  fontSize: "0.8rem"
-};
-
-const linkStyle = {
-  color: "#2d2d2d",
-  fontSize: "0.8rem"
+const rowStyle = {
+  margin: "auto",
+  textAlign: "center"
 };
 
 const parStyle = {
-  color: "#2d2d2d",
-  textAlign: "justify",
-  margin: "20px",
-  fontSize: "0.9rem",
-  maxWidth: "500px"
-}
+  margin: "auto",
+  maxWidth: "510px",
+  padding: "10px",
+  textAlign: "center"
+};
 
 const twitterStyle = {
-  margin: "20px"
-}
+  margin: "auto",
+  textAlign: "center",
+  maxWidth: "460px",
+  padding: "10px",
+};
 
 const introStyle = {
   color: "#2d2d2d",
   textAlign: "justify",
-  margin: "20px",
-  fontSize: "0.9rem",
-  maxWidth: "1000px"
+  fontSize: "1.2rem",
+  maxWidth: "1000px",
+  padding: '10px'
 }
+
 
 class Introduction extends Component {
   render() {
     return (
       <div>
-        <p style={introStyle}>
-          I am a Senior Engineer working in Arup Building Performance and Systems, London. I have experience providing assurance in building performance and digital projects for real estate organisations. I have previously held positions as a developer of Executive Education in Internet-of-Things for the University of Cambridge and as a Skills Manager for the Building Performance and Systems discipline globally across Arup.
-        </p>
-        <Row>
+        <div class="gray-container">
+          <p style={introStyle}>
+            I am a Senior Engineer working in Arup Building Performance and Systems, London. I have experience providing assurance in building performance and digital projects for real estate organisations. I have previously held positions as a developer of Executive Education in Internet-of-Things for the University of Cambridge and as a Skills Manager for the Building Performance and Systems discipline globally across Arup.
+          </p>
+        </div>
+        <Row style={rowStyle}>
           <Col style={parStyle}>
-            <p>
-              Professionally qualified as a Licensed Teacher, with a PhD from the University of Cambridge, a Chartered Engineer and a Member of CIBSE.
-              <br/><br/>
-              I am Director of REALFEED, a micro-sized business that publishes web insight.
-              <br/><br/>
-              I volunteer as referee for six scientific journals published by Elsevier.
-            </p>
             <ul>
-              <li class="nav-item">
-                <a href="/professional-cv.pdf" class="nav-link">Professional CV</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://arup.com" class="nav-link">Ove Arup and Partners</a>
-              </li>
+              <div class="blue-container">
+                <img src={`${process.env.PUBLIC_URL}/Ove_Arup.jpeg`} alt="Ove Arup"></img>
+                <br></br>
+                <br></br>
+                <a href="https://arup.com"><font color="DodgerBlue" size="1.2rem">OVE ARUP AND PARTNERS INTERNATIONAL LTD.</font></a>
+              </div>
             </ul>
-            <iframe width="480" height="270" src="https://www.youtube.com/embed/btxQAPmGOxU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="orange-container">
+              <iframe width="480" height="270" src="https://www.youtube.com/embed/btxQAPmGOxU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
           </Col>
           <Col style={twitterStyle}>
-            <Timeline
-              dataSource={{
-                url: "https://twitter.com/aidanparkinson?ref_src=twsrc%5Etfw"
-              }}
-              options={{
-                height: "530",
-                width: "400"
-              }}
-            />
+            <div class="red-container">
+              <a href={`${process.env.PUBLIC_URL}/professional-cv.pdf`}><font color="Tomato" size="1.2rem">PROFESSIONAL CV</font></a>
+            </div>
+            <br></br>
+            <div class = "green-container">
+              <Timeline
+                dataSource={{
+                  url: "https://twitter.com/aidanparkinson?ref_src=twsrc%5Etfw"
+                }}
+                options={{
+                  height: "530",
+                  width: "400"
+                }}
+              />
+            </div>
           </Col>
         </Row>
       </div>
